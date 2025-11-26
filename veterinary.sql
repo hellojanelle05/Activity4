dfdb25ab707a24f6285f0711d979edc9733a6725
hellojanelle05 <myjanelledrive@gmail.com>
2025-11-26 17:33:18 +0800

    feat/create-table-owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE veterinary;
+
+-- DON'T FORGET TO SAVE BEFORE ADDING TO GIT TRACK CHANGES!
+
+CREATE TABLE owners(
+    ownerid INT PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
